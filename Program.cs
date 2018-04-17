e309553c60c2c6a1f91fd68283e6ce72163ca49b
Qapples <34142272+Qapples@users.noreply.github.com>
2018-04-17 16:40:09 -0400

    added 20 minute delete r ole

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using System.Threading;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Net;
@@ -14,27 +15,69 @@ using Discord.Commands;
 using System.Collections.Specialized;
 namespace SCPFBot
 {
+    public class Timer
+    {
+        public static List<TimeObject> Time = new List<TimeObject>();
+    }
+    public class TimeObject
+    {
+        public int Time { get; set; }
+        public IGuildUser user { get; set; }
+        public TimeObject(int time, IGuildUser user)
+        {
+            this.Time = time;
+            this.user = user;
+        }
+    }
     class Program
     {
+        public Timer time;
         public CommandService commands;
         public DiscordSocketClient client;
         private IServiceProvider services;
+        static string key = "";
         static void Main(string[] args)
         {
+            Console.WriteLine("Insert Key.");
+            key = Console.ReadLine();
             new Program().Start().GetAwaiter().GetResult();
         }
         public async Task Start()
         {
+            Thread thread = new Thread(TimerThread);
+            thread.Start();
             client = new DiscordSocketClient();
             commands = new CommandService();
             services = new ServiceCollection().BuildServiceProvider();
             client.Log += Log;
             await CommandRegiester();
-            await client.LoginAsync(TokenType.Bot, "");
+            await client.LoginAsync(TokenType.Bot, key);
             await client.StartAsync();
 
 
             await Task.Delay(-1);
+        }
+        public static void TimerThread()
+        {
+            while (true)
+            {
+                for (int i = 0; i < Timer.Time.ToArray().Length; i++)
+                {
+                    int time = Timer.Time[i].Time;
+                    var user = Timer.Time[i].user;
+                    if (time >= 1200000)
+                    {
+                        var role = user.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "Looking For Game");
+                        user.RemoveRoleAsync(role);
+                        Timer.Time.RemoveAt(i);
+                    }
+                    else
+                    {
+                        Timer.Time[i].Time += 1000;
+                    }
+                }
+                Thread.Sleep(1000);
+            }
         }
         private Task Log(LogMessage Arg)
         {
